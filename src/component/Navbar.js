4618a59df7a612e14793d24cacfd7dbc04c0c5da
tommy <odubatomiwa@gmail.com>
2023-02-06 23:36:22 +0100

    fixed the mobile navbar menu

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -67,13 +67,13 @@
                 <a  href="#home">Home</a>
                 </li>
                 <li>
-                <a href="/about">About</a>
+                <a href="#about">About</a>
                 </li>
                 <li>
-                <a href="/project">Project</a>
+                <a href="#project">Project</a>
                 </li>
                 <li>
-                <a href="/contact">Contact</a>
+                <a href="#contact">Contact</a>
                 </li>
                 {/* <DarkToggleMenu /> */}
             </ul>
